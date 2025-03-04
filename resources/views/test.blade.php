1bfaa44c505deff085649b2f99da8ea98f17ec08
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-03-04 21:09:26 +0200

    عمل تقسم الصفحات والوراثة

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('title')
+test page
+@endsection
+@section('content')
+mahdii
+@endsection
+
+@section('contentheaderlink')
+<a href="mahdi.com"></a>
+@endsection
+
+@section('contentheaderactive')
+mahdii
+@endsection
